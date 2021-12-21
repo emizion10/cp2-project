9a0fb1485903beab53884d5d6733d689cb1537b8
Amal S <amals@Amals-MacBook-Air.local>
2021-12-22 00:27:04 +0530

    optimise stretching logic

--- stretch_molecule.py
+++ stretch_molecule.py
@@ -1,5 +1,6 @@
 from readxyz import readxyz_function
 from writexyz import writexyz_function
+import math
 
 
 def stretch_molecule(filename):
@@ -13,13 +14,18 @@ def stretch_molecule(filename):
     coordinates = result[3]
 
     # stretch coordinates
-    coordinate_diff = [(coordinates[atom1][i]-coordinates[atom2][i])
-                       for i in range(3)]
-    bond_length = (
-        coordinate_diff[0]**2 + coordinate_diff[1]**2+coordinate_diff[2]**2)**1/2
-    coordinates[atom2] = [coordinates[atom2][i] + delta *
-                          (coordinate_diff[i])/bond_length for i in range(3)]
+
+    coordinate_diff = []
+    for i in range(3):
+        coordinate_diff.append(coordinates[atom1][i]-coordinates[atom2][i])
+
+    bond_length = math.sqrt(coordinate_diff[0]**2 + coordinate_diff[1]
+                            **2 + coordinate_diff[2]**2)
+
+    for i in range(3):
+        coordinates[atom2][i] = coordinates[atom2][i] + (delta *
+                                                         coordinate_diff[i])/bond_length
+
     streched_result = n_atoms, result[1], result[2], coordinates
 
     writexyz_function(streched_result, 'streched_'+filename)
-
